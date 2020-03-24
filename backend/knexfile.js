0c35d62147cc970b901aca9c3427c798a83d0917
Alex Botelho <alexbotelho1@hotmail.com>
2020-03-24 18:07:50 -0300

    Dia 2

---
+++ backend/knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host:     '127.0.0.1',
+      user:     'root',
+      password: '',
+      database: 'omnistack'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    }
+  },
+
+  staging: {
+    client: 'mysql',
+    connection: {
+      host:     '127.0.0.1',
+      user:     'root',
+      password: '',
+      database: 'omnistack'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ backend/src/routes.js
@@ -0,0 +1,28 @@
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+const express = require('express');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile/:id', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
