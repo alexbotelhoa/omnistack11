fa899145fd71e27c1bcf3e5a8995a213f3184a7e
Alex Botelho <alexbotelho1@hotmail.com>
2020-06-15 15:21:01 -0300

    Licença

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import Incidents from './pages/Incidents';
 import Details from './pages/Details';
 
